4289bb48d8891c56c821d3f7625e878eee6dd17f
Simon Haines <nimon@hotmail.co.uk>
2024-06-04 13:26:51 +0100

    Update subtract.js

--- src/subtract.js
+++ src/subtract.js
@@ -1,7 +1,9 @@
 /**
- * Returns the positive difference
+ * Returns the positive difference (or zero)
  * subtract(5, 2) => 3
  * subtract(3, 7) => 4
+ * subtract(5, 5) => 0
+ * subtract(-2, 4) => 6
  */
 function subtract(x, y) {
   if (x < y) {
